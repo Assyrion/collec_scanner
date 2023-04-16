e7ce1ae8572ba6db8d526119990ebeb7b2c8f199
Assyrion <kilikstaff@hotmail.fr>
2023-04-16 14:55:20 +0200

    Handle updating cover when already existing

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@
 #include "gamedata.h"
 #include "global.h"
 
+const QRegularExpression re("href=\"([^\"]+\\.png)\">.*?(\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2})");
+
 void downloadCovers(QObject* dialog)
 {
     QDir toDir(PICPATH_ABS);
@@ -29,6 +31,7 @@ void downloadCovers(QObject* dialog)
     toDir.setFilter(QDir::Files | QDir::NoSymLinks);
     toDir.setNameFilters(QStringList() << "*.png");
     int local_count = toDir.entryList().count();
+    int count = 0;
 
     QUrl url(REMOTE_PIC_PATH);
     QNetworkAccessManager manager;
@@ -46,41 +49,49 @@ void downloadCovers(QObject* dialog)
 
             QMetaObject::invokeMethod(dialog, "setMaxValue", Q_ARG(QVariant, remote_count - local_count));
 
-            if(local_count < remote_count) {
-                int count = 0;
-                QStringList files = data.split("\n", Qt::SkipEmptyParts);
-                const QRegularExpression re("href=\"([^\"]+\\.png)\">");
-                for (const QString& file : files) {
-                    QRegularExpressionMatch match = re.match(file);
-                    if(match.hasMatch()) {
-                        QString fileName = match.captured(1);
-                        QString toPath = toDir.absolutePath()
-                                + QDir::separator()
-                                + fileName;
-                        if(!QFile::exists(toPath)) {
-                            QUrl fileUrl(url.toString() + fileName);
-                            QNetworkRequest fileRequest(fileUrl);
-                            QNetworkReply *fileReply = manager.get(fileRequest);
-                            while (!fileReply->isFinished()) {
-                                qApp->processEvents();
-                            }
-                            // Vérification du code de réponse
-                            if (fileReply->error() == QNetworkReply::NoError) {
-                                // Enregistrement du fichier sur le disque
-                                QFile file(toPath);
-                                if (file.open(QIODevice::WriteOnly)) {
-                                    file.setPermissions(QFile::WriteOwner | QFile::ReadOwner);
-                                    file.write(fileReply->readAll());
-                                    file.close();
+            QStringList htmlLineList = data.split("\n", Qt::SkipEmptyParts);
 
-                                    QMetaObject::invokeMethod(dialog, "setValue", Q_ARG(QVariant, ++count));
-                                }
-                            } else {
-                                // Traitement de l'erreur
-                                qDebug() << "Erreur lors du téléchargement du fichier" << fileName << ":" << fileReply->errorString();
-                            }
-                            fileReply->deleteLater();
+            for (const QString& htmlLine : htmlLineList) {
+                QRegularExpressionMatch match = re.match(htmlLine);
+
+                if(match.hasMatch()) {
+                    QString remoteFileName(match.captured(1));
+                    QDateTime remoteCreationDate(QDateTime::fromString(match.captured(2), Qt::ISODate));
+
+                    QString localPath = toDir.absolutePath()
+                                     + QDir::separator()
+                                     + remoteFileName;
+
+                    QDateTime localModifiedDate(remoteCreationDate);
+
+                    if(QFile::exists(localPath)) {
+                        QFileInfo localFileInfo(localPath);
+                        localModifiedDate = localFileInfo.lastModified();
+                    }
+
+                    if(!QFile::exists(localPath) || (remoteCreationDate > localModifiedDate)) {
+                        QUrl fileUrl(url.toString() + remoteFileName);
+                        QNetworkRequest fileRequest(fileUrl);
+                        QNetworkReply *fileReply = manager.get(fileRequest);
+                        while (!fileReply->isFinished()) {
+                            qApp->processEvents();
                         }
+                        // Vérification du code de réponse
+                        if (fileReply->error() == QNetworkReply::NoError) {
+                            // Enregistrement du fichier sur le disque
+                            QFile file(localPath);
+                            if (file.open(QIODevice::WriteOnly)) {
+                                file.setPermissions(QFile::WriteOwner | QFile::ReadOwner);
+                                file.write(fileReply->readAll());
+                                file.close();
+
+                                QMetaObject::invokeMethod(dialog, "setValue", Q_ARG(QVariant, ++count));
+                            }
+                        } else {
+                            // Traitement de l'erreur
+                            qDebug() << "Erreur lors du téléchargement du fichier" << remoteFileName << ":" << fileReply->errorString();
+                        }
+                        fileReply->deleteLater();
                     }
                 }
             }
