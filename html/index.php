c66557bb70a45a1dab5af96e599e67a25c953580
Assyrion <kilikstaff@hotmail.fr>
2023-05-09 14:35:24 +0200

    Page template

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CollecScanner - Présentation</title>
+</head>
+<body>
+	<h1>CollecScanner</h1>
+	<p>Bienvenue sur le site de présentation de CollecScanner, l'application mobile pour scanner et gérer votre collection.</p>
+	
+	<h2>Téléchargement</h2>
+	<p>CollecScanner est disponible en téléchargement pour Android et iOS :</p>
+	<ul>
+		<li><a href="https://play.google.com/store/apps/details?id=com.example.collecscanner">Google Play Store</a></li>
+		<li><a href="https://apps.apple.com/us/app/collecscanner/id123456789">App Store</a></li>
+	</ul>
+	
+	<p>Merci d'utiliser CollecScanner !</p>
+</body>
+</html>
