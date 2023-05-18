892a4a5a228289d81c9b7fb8f65ee078654e9faa
Assyrion <kilikstaff@hotmail.fr>
2023-05-18 02:43:53 +0200

    Add button to download exe

--- html/index.php
+++ html/index.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>CollecScanner - Présentation</title>
-</head>
-<body>
-	<h1>CollecScanner</h1>
-	<p>Bienvenue sur le site de présentation de CollecScanner, l'application mobile pour scanner et gérer votre collection.</p>
-	
-	<h2>Téléchargement</h2>
-	<p>CollecScanner est disponible en téléchargement pour Android et iOS :</p>
-	<ul>
-		<li><a href="https://play.google.com/store/apps/details?id=com.example.collecscanner">Google Play Store</a></li>
-		<li><a href="https://apps.apple.com/us/app/collecscanner/id123456789">App Store</a></li>
-	</ul>
-	
-	<p>Merci d'utiliser CollecScanner !</p>
-</body>
+  <head>
+    <title>CollecScanner</title>
+    <style>
+      body {
+        background-color: #333333;
+      }
+      h1 {
+        color: #ffffff;
+        text-align: center;
+      }
+      .button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+      }
+      .button-container button {
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 24px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 8px;
+      }
+      .button-container button:first-child {
+        margin-right: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>CollecScanner</h1>
+    <div class="button-container">
+      <a href="apk/collecscanner.apk" download>
+        <button>Télécharger APK</button>
+      </a>
+      <a href="exe/collecscanner.zip" download>
+        <button>Télécharger EXE</button>
+      </a>
+    </div>
+  </body>
 </html>
